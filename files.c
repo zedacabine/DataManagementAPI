0ba723278a42b47eafedf14a66b7a1275684b3b2
zedacabine <epilif81_@hotmail.com>
2015-01-19 15:52:08 +0000

    file atualizado

--- files.c
+++ files.c
@@ -4,27 +4,27 @@
 
     FILE *ap_ficheiro;
    
-    void readFile(char *nome, int tamArrayPergunta, Pergunta *perguntas, int *contador){    
+    void readFile(char *nome, Class class, unsigned int tam_max){    
     ap_ficheiro = fopen(nome, "r+"); // Abre o ficheiro
     if (!ap_ficheiro){ // Se o apontador devolvido for inválido ocorreu um erro na leitura
-        printf("\n IMPOSSIVEL ABRIR O FICHEIRO.");
-        printf("\n\n ");
+       // printf("\n IMPOSSIVEL ABRIR O FICHEIRO.");
+       // printf("\n\n ");
     } else {
-        *contador = fread(perguntas, sizeof (Pergunta), tamArrayPergunta, ap_ficheiro); // Lê a informação do ficheiro e armazena no array
-     
+        *(class.elements) = fread(class.data, class.StructTypeSize, tam_max, ap_ficheiro); // Lê a informação do ficheiro e armazena no array
         fclose(ap_ficheiro); // Fecha o ficheiro
-        printf("\n O FICHEIRO FOI LIDO COM SUCESSO.\n\n ");
+       // printf("\n O FICHEIRO FOI LIDO COM SUCESSO.\n\n ");
     }
     }
 
-    void writeFile(char *nome, Pergunta *perguntas, int contador){     
-    ap_ficheiro = fopen(nome, "w"); // Abre o ficheiro  em modo de escrita
+    
+    void writeFile(char *nome, Class class){   
+        ap_ficheiro = fopen(nome, "w"); // Abre o ficheiro  em modo de escrita
     if (!ap_ficheiro) {
-        printf("\n IMPOSSIVEL ABRIR O FICHEIRO."); // Se o apontador devolvido for inválido ocorreu um erro na leitura
-        printf("\n\n ");
+        //printf("\n IMPOSSIVEL ABRIR O FICHEIRO."); // Se o apontador devolvido for inválido ocorreu um erro na leitura
+        //printf("\n\n ");
     } else {
-        fwrite(perguntas, sizeof (Pergunta), contador, ap_ficheiro); // Guarda a informação do array no ficheiro 
+        fwrite(class.data, class.StructTypeSize, *(class.elements), ap_ficheiro); // Guarda a informação do array no ficheiro 
         fclose(ap_ficheiro); // Fecha o ficheiro
-        printf("\n O FICHEIRO FOI GUARDADO COM SUCESSO.\n\n ");
+        //printf("\n O FICHEIRO FOI GUARDADO COM SUCESSO.\n\n ");
     }
     }
