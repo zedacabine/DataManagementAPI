597d6ae6b12b2b8733d9d30cff63abb56275f768
zedacabine <epilif81_@hotmail.com>
2015-01-21 09:07:49 +0000

    jogo

--- Jogo.c
+++ Jogo.c
@@ -1,7 +1,13 @@
 //#include "DataManagement.h"
 #include "Jogo.h"
 #include "Dificuldade.h"
+#include "Pergunta.h"
+#include "Resposta.h"
 #include <string.h>
+#include <time.h>
+#include "Categoria.h"
+#include "Jogadas.h"
+#include <unistd.h>
 
 void inserirJogo(Class *jogoClass) {
 
@@ -40,60 +46,156 @@ void pesquisarJogos(Class jogoClass, const unsigned int campo, void *valorPesqui
 }
 
 void modoNormal(void **arrayClass) {
+    int id_local_user = 2;
+
+
+    Class *perguntaClass;
+    perguntaClass = arrayClass[0];
+
+    Class *categoriaClass;
+    categoriaClass = arrayClass[1];
+
+    Class *respostaClass;
+    respostaClass = arrayClass[2];
 
     Class *dificuldadeClass;
     dificuldadeClass = arrayClass[3];
-    
+
+    Class *jogadaClass;
+    jogadaClass = arrayClass[5];
+
     Class *jogoClass;
     jogoClass = arrayClass[6];
 
-    int id_local_user = 2;
+    Pergunta *perguntas;
+    perguntas = perguntaClass->data;
+    perguntas;
 
-    //listarJogos(jogoClass);
     Jogo *jogos;
     jogos = jogoClass->data;
     jogos;
+
+    Dificuldade *dificuldades;
+    dificuldades = dificuldadeClass->data;
+    dificuldades;
+
+    Resposta *respostas;
+    respostas = respostaClass->data;
+    respostas;
+
+    Categoria *categorias;
+    categorias = categoriaClass->data;
+    categorias;
+
+    Jogada *jogadas;
+    jogadas = jogadaClass->data;
+    jogadas;
+
+
     //------------------------------------Criar Jogo------------------------------//
     jogos[*(jogoClass->elements)].id_jogo = *(jogoClass->elements) + 1;
     jogos[*(jogoClass->elements)].id_utilizador = id_local_user;
     *(jogoClass->elements) = *(jogoClass->elements) + 1;
 
-    Dificuldade *dificuldades;
-    dificuldades = dificuldadeClass->data;
-    dificuldades;
-    
-    
-    
-    listarDificuldades();
+    int camposEditarPergunta[] = {PERGUNTA};
+    int valor = 1;
+    int numeroResultados;
+    char sinal[2 + 1];
+    strcpy(sinal, "==");
+    int * resultPesquisa;
+    resultPesquisa = pesquisarPerguntas(perguntaClass, ID_DIFICULDADE_PERGUNTAS, &valor, &numeroResultados, sinal);
+    //filtrarPerguntas(perguntaClass,resultPesquisa,numeroResultados,camposEditarPergunta,2);
+    int *res_rand = 0;
+    res_rand = randomize(resultPesquisa, 6, numeroResultados);
+    unsigned int r = 0;
+    clearscreen();
+
+ 
+    //------------------------------------------------iniciar Perguntas ------------------------------//
+    for (r = 0; r < 6 - 1; r++) {
+          
+        
+        
+        filtrarPergunta(perguntaClass, *(res_rand + r), camposEditarPergunta, 1);
+        int camposEditarRespostas[] = {RESPOSTA};
+        int valorResposta = (*(res_rand + r)) + 1;
+        int unsigned numeroResultadosRespostas;
+        char sinalRespostas[2 + 1];
+        strcpy(sinalRespostas, "==");
+        int * resultPesquisaResposta;
+
+        resultPesquisaResposta = pesquisarRespostas(respostaClass, ID_PERGUNTA_RESPOSTA, &valorResposta, &numeroResultadosRespostas, sinalRespostas);
+        unsigned int u;
+
+//------------------------------------------------iniciar respostas----------------------------//
+        for (u = 0; u < numeroResultadosRespostas; u++) {
+            printf("%d: ", respostas[(*(resultPesquisaResposta + u))].id_resposta);
+            filtrarResposta(respostaClass, (*(resultPesquisaResposta + u)), camposEditarRespostas, 1);
+        }
+
+        time_t inicio;
+        time(&inicio);
+        puts("Insira uma opcao: ");
+        int solucao = 0;
+        readInt(&solucao);
+
+        time_t fim;
+        time(&fim);
+        unsigned int diferencaTempo;
+        diferencaTempo = difftime(fim, inicio);
+        int camposEditarSolucao[] = {ID_RESPOSTA_CERTA_PERGUNTAS};
+        int valorSolucao = solucao;
+        int unsigned numeroResultadosSolucao;
+        char sinalSolucao[2 + 1];
+        strcpy(sinalSolucao, "==");
+        int * resultPesquisaSolucao;
+
+        jogadas[*(jogadaClass->elements)].id_jogada = *(jogadaClass->elements) + 1;
+        jogadas[*(jogadaClass->elements)].id_utilizador = id_local_user;
+        jogadas[*(jogadaClass->elements)].id_jogo = *(jogoClass->elements);
+        jogadas[*(jogadaClass->elements)].tempo = diferencaTempo;
+        jogadas[*(jogadaClass->elements)].id_resposta = solucao;
+        jogadas[*(jogadaClass->elements)].id_pergunta = valorResposta;
 
 
 
-    printf("Tamanho do array -> %d", *(jogoClass->elements));
+        pesquisarPerguntas(perguntaClass, ID_RESPOSTA_CERTA_PERGUNTAS, &valorSolucao, &numeroResultadosSolucao, sinalSolucao);
+
+        if (numeroResultadosSolucao == 0) {
+            jogadas[*(jogadaClass->elements)].pontuacao = 0;
+            *(jogadaClass->elements) = *(jogadaClass->elements) + 1;
+            puts("Resposta Errada Perdeste");
+            usleep(1000000);
+            menu_admin(arrayClass);
+        }
+        if (numeroResultadosSolucao == 1) {
+            jogadas[*(jogadaClass->elements)].pontuacao = (dificuldades[valor].pontuacao_dificuldade + categorias[perguntas[(*(res_rand + r))].id_categoria].pontuacao_categoria)-(diferencaTempo);
+            puts("Resposta Correcta");
+            printf("%d", (*(res_rand + r)));
+            printf("Ganhou: %d pontos", jogadas[(*(jogadaClass->elements))].pontuacao);
+            *(jogadaClass->elements) = *(jogadaClass->elements) + 1;
+
+            usleep(1000000);
+        }
 
 
+        char NomeFicheiro[SHORT_STRING];
+        strcpy(NomeFicheiro, "jogadas.txt");
+        writeFile(NomeFicheiro, jogadaClass);
+        getchar();
+
+        //listarPergunta(perguntaClass, *(res_rand+r));
+    }
     char NomeFicheiro[SHORT_STRING];
     strcpy(NomeFicheiro, "jogos.txt");
     writeFile(NomeFicheiro, jogoClass);
 
-    listarJogos(jogoClass);
-
-
-    //inserirJogo(jogoClass);
-
-
+    //printf("Tamanho do array -> %d", *(jogoClass->elements));
     //char NomeFicheiro[SHORT_STRING];
     //strcpy(NomeFicheiro, "jogos.txt");
     //writeFile(NomeFicheiro, jogoClass);
 
-
-
-    puts("                       Modo Normal                               \n");
-    puts("                                                                 \n");
-    puts("                                                                 \n");
-
-    voltar();
-
-
+    //inserirJogo(jogoClass);
 }
 
 void modoDificuldade() {
