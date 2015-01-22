6db2089f2d917e141f1da8e5e8641b4df58c282a
ivo <ivo@ivo-All-Series>
2015-01-22 23:12:54 +0000

    coments

--- DataManagement.h
+++ DataManagement.h
@@ -30,10 +30,12 @@ extern "C" {
 #define SHORT_STRING 28
 #define MEDIUM_STRING 80
 #define LONG_STRING 260 
-    
- #define MAX_RESULTS 50
 
+#define MAX_RESULTS 50
 
+    /*
+     * Data Types
+     */
     typedef enum {
         INT,
         SHORT,
@@ -47,7 +49,10 @@ extern "C" {
         STRUCT,
         BOOL
     } DataType;
-
+    
+    /*
+     * Type of request ( functionality not implemented )
+     */
     typedef enum {
         CREATE,
         UPDATE,
@@ -55,6 +60,15 @@ extern "C" {
         DELETE
     } RequestType;
 
+    /*
+     * @atribute name - Class name ( name of the table )
+     * @atribute primaryKeyField - Number of primary key field
+     * @atribute data - memory adress of the struct that contains all the class registries   
+     * @atribute StructTypeSize - Bytes size of the data struct type
+     * @atribute auxStruct - Memory adress of the struct that contains all the fields information
+     * @atribute elements - Memory adress  of the variable that contains the actual number of data struct registries ( table registries number )
+     * @atribute aliasField - Number of the auxStruct field that is going to be displayed if a foreign key requested
+     */
     typedef struct {
         char name[20];
         unsigned short primaryKeyField;
@@ -66,6 +80,23 @@ extern "C" {
         unsigned int aliasField;
     } Class;
 
+    /*
+     *@atribute fieldName- Name of the field ( table field )
+     *@atribute alias - Name that is going to be displayed on lists 
+     *@atribute  type - Field data type
+     *@atribute required - If the field is required or if can be leaved in white
+     *@atribute unique - If the field can have replications( functionality not implemented ) 
+     *@atribute autoIncrement - If the field is auto incremental 
+     *@atribute primaryKey - If the field is a primary key of the table
+     *@atribute foreignKey - If the field is a foreign key and have relation with other table
+     *@atribute parentClass -Memory adress of the Class (table) that is related too
+     *@atribute parentPrimaryKey - Number of the primary key field that is going to be relationated
+     *@atribute step - Number of step if the field is auto incremental ex: step 1: 1,2,3,4 ; step 2 :1,3,5,7 ;
+     *@atribute sizeBytes - Bytes size of the field type
+     *@atribute minSize - Minimum size of the field ( functionality not implemented )
+     *@atribute maxSize - Maximum size of the field ( functionality not implemented )   
+     *@atribute substruct - Memory adress of the Class (case this field is a struct ) ( functionality not implemented )
+     */
     typedef struct {
         char fieldName[MEDIUM_STRING];
         char alias[MEDIUM_STRING];
@@ -83,7 +114,12 @@ extern "C" {
         unsigned int maxSize;
         void *substruct;
     } FieldAux;
-
+    
+    
+    /*
+     *Interface implementation ( not completed )
+     */
+    
     typedef struct {
         void (*listRegistry)(void *, FieldAux *, unsigned);
         void (*fullList)(void *, const unsigned short, const unsigned int, FieldAux *, const unsigned int);
@@ -104,7 +140,7 @@ extern "C" {
         void (*delete)();
         List list;
     } crud;
-
+    
     //Print interface
     void printString(const void *s);
     void printInt(const void *i);
